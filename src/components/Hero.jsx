c8779a8547cb22e173cb3710e6e786f4ea44260a
ShokatShahinjsx <shokatshahin50@gmail.com>
2025-02-20 01:55:46 +0400

    New Updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { motion } from 'framer-motion'
 import { styles } from '../style'
 import { ComputersCanvas } from './canvas'
+import gsap from "gsap";
+
+
 
 const Hero = () => {
+  const ballRef = useRef(null)
+  const textRef = useRef(null)  
+  
+  useEffect(() => {
+    if (ballRef.current && textRef.current) {
+      gsap.fromTo(
+        ballRef.current,
+        { opacity: 0, y: 400 },
+        { delay: 1.5, opacity: 1, y: 0, duration: 2 }
+      );
+
+      gsap.fromTo(
+        textRef.current,
+        { opacity: 0, y: 200 },
+        { delay: 1.5, opacity: 1, y: 0, duration: 1 }
+      );
+    }
+  }, []);
+
   return (
       <section className='relative w-full h-screen mx-auto bg-hero-pattern bg-cover bg-center bg-no-repeat'>
         <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
           <div className='flex flex-col justify-center items-center mt-5'>
-            <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+            <div className='w-5 h-5  rounded-full bg-[#7764a1]' ref={ballRef}/>
             <div className='w-1 sm:h-80 h-40 violet-gradient'/>
           </div>
             <div>
-              <h1 className={`${styles.heroHeadText}`}>Hi I'm<span className='text-[#915eff]'> Shokat</span></h1>
+              <div className='overflow-hidden'>
+                <h1 className={`${styles.heroHeadText}`}>Hi I'm <div ref={textRef} className="inline-block">
+                <span className="text-[#915eff]">Shokat</span>
+              </div></h1>
+              </div>
               <p className={`${styles.heroSubText} mt-2 text-white-100 `}>I Devlop 3D Visuals , user <br className='sm:block hidden' /> interfaces and web applications</p>
             </div>
         </div>

bbd966dfac33d1cab893ea9977ef29d0bb3410c0
ShokatShahinjsx <shokatshahin50@gmail.com>
2025-02-20 15:56:46 +0400

    New GLTF

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,13 +4,14 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from "../Loader"
 
 const Computers = ({isMobile}) => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  const computer = useGLTF('./gaming_setup_v2_low-poly (1)/scene.gltf')
   return (
     <mesh>
       <hemisphereLight intensity={2} groundColor="black" />
       <spotLight position={[-20,50,10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
+      <spotLight position={[20,50,10]} angle={3} penumbra={1} intensity={100} castShadow shadow-mapSize={1024} />
       <pointLight intensity={2.5} />
-      <primitive object={computer.scene} scale={isMobile ? 0.6 : 0.75} position={isMobile ? [0, -3,-1] : [0, -3.25, -1.5]} rotation={[-0.01,-0.2,,-0.1]} />
+      <primitive object={computer.scene} scale={isMobile ? 0.6 : 1} position={isMobile ? [0, -3,-1] : [0, -4,0]} rotation={[-0.01,-0.2,,-0.1]} />
     </mesh>
   )
 }
@@ -43,7 +44,7 @@ const ComputersCanvas = () => {
     <Canvas 
       frameloop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [15, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
